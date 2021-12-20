416812bc5e651f7eb468f46c693a796006c4e4c8
Sean Weaver <weaversa@gmail.com>
2021-12-20 16:48:10 -0500

    Added a script to demonstrate running with Docker

---
+++ scripts/dockerrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIRYOUTH=$(realpath $1)
+DIRWORK=$(realpath $2)
+
+docker run --rm -it \
+       --mount type=bind,src=$DIRYOUTH,dst=$DIRYOUTH \
+       --mount type=bind,src=$DIRWORK,dst=$DIRWORK \
+       --mount type=bind,src=$(pwd),dst=/output \
+       --workdir=$(pwd) youthworks $1 $2
+
+echo "If successful, results were written to the file \`YouthworksMatching.xlsx\` in the current directory."
