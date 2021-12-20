af609e49eb88b8933b0fdd9d1c403b1378f4288b
Sean Weaver <weaversa@gmail.com>
2021-12-20 16:52:10 -0500

    specializing docker scripts to weaversa

---
+++ scripts/dockerbuild.sh
@@ -0,0 +1,1 @@
+docker build --no-cache -t weaversa/youthworks -f YouthWorks.Dockerfile .
--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -7,6 +7,6 @@ docker run --rm -it \
        --mount type=bind,src=$DIRYOUTH,dst=$DIRYOUTH \
        --mount type=bind,src=$DIRWORK,dst=$DIRWORK \
        --mount type=bind,src=$(pwd),dst=/output \
-       --workdir=$(pwd) youthworks $1 $2
+       --workdir=$(pwd) weaversa/youthworks $1 $2
 
 echo "If successful, results were written to the file \`YouthworksMatching.xlsx\` in the current directory."
